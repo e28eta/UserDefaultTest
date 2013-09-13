14c538cec3fe91ea782f6ddf20a36f8f2d1f680b
Daniel Jackson <e28eta@yahoo.com>
2013-09-12 18:38:48 -0700

    Initial Commit

---
+++ UserDefaultTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UserDefaultTest
+//
+//  Created by Jackson, Daniel on 9/12/13.
+//  Copyright (c) 2013 Intuit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
